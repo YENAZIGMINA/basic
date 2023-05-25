3515ee19e601584bc833de8ccfd886c85f340003
YENAZIGMINA <129706758+YENAZIGMINA@users.noreply.github.com>
2023-05-25 19:02:04 +0900

    Add files via upload

--- example/15.cider/main.js
+++ example/15.cider/main.js
@@ -1,23 +1,19 @@
-let hCont = $('.history_slide')
-let hBtn = $('.history_btn li')
+let hCont=$('.history_slide');
+let hBtn=$('.history_btn li');
 let hIndex;
 
-
 hCont.slick({
+});
 
-}); 
-
-
-/* ✔ 버튼을 클릭하면 해당 index를 찾아서 가기 */
+// 버튼을 클릭하면 해당 index를 찾아서 가기
 hBtn.click(function(e){
     e.preventDefault();
-
-    let target = $(this); //클릭한 그것 선택
-    let index = target.index(); // 클릭한 그것의 index 번호
+    let target=$(this);//클릭한 그것
+    let index = target.index();//클릭한 그것의 index번호
     //console.log(index)
 
     if(index == 0){
-        hIndex = 1
+        hIndex = 0
     }else if(index == 1){
         hIndex = 4
     }else if(index == 2){
@@ -28,16 +24,12 @@ hBtn.click(function(e){
         hIndex = 16
     }
     console.log(hIndex)
-    hCont.slick('slickGoTo',hIndex); //slick-get~ gitHUB- 메소드 복붙 / 
-    //✔ hIndex로 slickGoTo 된다는 의미
-
+    hCont.slick('slickGoTo',hIndex);
 
     hBtn.removeClass('active')
     target.addClass('active')
 })
 
-
-/* 깃헙 event - slide change 복붙 */
 hCont.on('beforeChange', function(event, slick, currentSlide, nextSlide){
     console.log(nextSlide);
     if(nextSlide>=0 && nextSlide<=3){
@@ -46,7 +38,7 @@ hCont.on('beforeChange', function(event, slick, currentSlide, nextSlide){
     }else if(nextSlide>=4 && nextSlide<=7){
         hBtn.removeClass('active')
         hBtn.eq(1).addClass('active')
-    }else if(nextSlide>=8 && nextSlide <= 11){
+    }else if(nextSlide>=8 && nextSlide<=11){
         hBtn.removeClass('active')
         hBtn.eq(2).addClass('active')
     }else if(nextSlide>=12 && nextSlide<=15){
@@ -56,39 +48,38 @@ hCont.on('beforeChange', function(event, slick, currentSlide, nextSlide){
         hBtn.removeClass('active')
         hBtn.eq(4).addClass('active')
     }
-    $('.history_page em').text(nextSlide + 1) //직접 작성
 
+    $('.history_page em').text(nextSlide + 1)
+  });
 
-});
+//   history_poster
+let posterBtn=$('.poster_btn ul li');
+let posterCont=$('.poster_cont .poster');
 
-
-
-//history_poster
-let posterBtn = $('.poster_btn ul li');
-let posterCont = $('.poster_cont .poster');
-
-//posterCont 모두 안보이게, 하지만 첫번째는 보이기
+//posterCont를 모두 안보이게 첫번째는 보이기
 posterCont.hide().eq(0).show();
-
 
 posterBtn.click(function(e){
     e.preventDefault();
-
     let target = $(this);
     let index = target.index();
     //console.log(index)
     posterCont.hide().eq(index).fadeIn();
-    posterCont.find('figure').removeClass('move');
-    posterCont.eq(index).find('figure').addClass('move');
+    posterCont.find('figure').removeClass('move')
+    posterCont.eq(index).find('figure').addClass('move')
 
-
-    posterBtn.removeClass('active');
-    target.addClass('active');
-
+    posterBtn.removeClass('active')
+    target.addClass('active')
 })
 
+// history_cider
+  $('.cider_bottle').slick({
+    dots:true,
+    centerMode:true,
+    centerPadding:"380px"
+  });
 
-
-$('.cider_bottle').slick({
+  //memory-slide
+  $('.memory-slider').slick({
     
   });
